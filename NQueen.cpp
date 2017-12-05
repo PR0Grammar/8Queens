3df278ac7d534c10aafad15c655bdbf39f8495cd
Pr0grammar <npatel3030@gmail.com>
2017-12-04 20:16:07 -0500

    Queens Problems

---
+++ NQueen.cpp
@@ -0,0 +1,67 @@
+/*
+ * NQueen.cpp
+ *
+ *  Created on: Nov 4, 2017
+ *      Author: nehal
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+bool check(int q[], int c){
+
+	for(int i = 0; i < c ; i++){
+		if(q[i] == q[c])
+			return false;
+		if( abs(q[i]-q[c]) == c - i )
+			return false;
+	}
+	return true;
+}
+
+int modularQueen(int n){
+	int* q = new int[n];
+	int solutionNum = 0;
+
+	int column = 0;
+
+	while (column >= 0){
+
+		if (column == n-1){
+			solutionNum++;
+			column--; //Solution found now backtrack everything
+		}
+
+		else
+			q[++column] = -1; //starts at 1 (this is what increments the column)
+
+			while (column >= 0){
+				q[column]++;
+
+				if(q[column] == n) //outside the board
+					column--; //backtrack
+				else if(check(q, column)){ //if queen placed checks out, break the loop
+					break;
+				}
+			}
+	}
+
+	delete []q;
+	return solutionNum;
+}
+
+int main(){
+	int n;
+	cout<<"Give me an integer: ";
+	cin>>n;
+
+
+	for(int i = 1; i <= n ; i++){
+		cout<<i<<". There are "<<modularQueen(i)<<" solutions to the " << i << " queens problem"<<endl;
+	}
+
+	return 0;
+}
+
